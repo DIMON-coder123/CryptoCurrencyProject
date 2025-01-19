2222c676cef800c35a7cc6497565a9efd0fd229e
DIMON-coder123 <d.dzhos@ukma.edu.ua>
2025-01-19 18:10:12 +0000

    Fix problem with email-validator

--- backend/src/JWT_settings.py
+++ backend/src/JWT_settings.py
@@ -3,7 +3,7 @@ from authx import AuthX, AuthXConfig
 
 config = AuthXConfig()
 config.JWT_SECRET_KEY = settings.JWT_KEY
-config.JWT_ACCESS_COOKIE_NAME = "acces  s_token"
+config.JWT_ACCESS_COOKIE_NAME = "access_token"
 config.JWT_TOKEN_LOCATION = ["cookies"]
 
 security = AuthX(config=config)
